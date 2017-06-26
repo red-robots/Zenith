e5cd9e5b0cf94ef7eb68e4da149541ab596020c6
reneeobrien <reneelobrien@gmail.com>
2017-06-26 14:38:38 -0400

    hamburger menu icon on responsive

--- header.php
+++ header.php
@@ -35,7 +35,7 @@
 			<div class="right-section">
 				<div class="menu">  
 					<nav id="site-navigation" class="main-navigation" role="navigation">
-						<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'acstarter' ); ?></button>
+						<i class="fa fa-bars fa-2x" aria-hidden="true"></i>
 						<?php wp_nav_menu( array( 'theme_location' => 'primary', 'menu_id' => 'primary-menu' ) ); ?>
 					</nav><!-- #site-navigation -->
 				</div><!--menu --> 
