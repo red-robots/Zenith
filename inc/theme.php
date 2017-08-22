b3e4e26082de251831415975d435831f6e254194
reneeobrien <reneelobrien@gmail.com>
2017-08-22 10:14:46 -0400

    styles

--- inc/theme.php
+++ inc/theme.php
@@ -13,10 +13,10 @@
 function my_login_logo() { ?>
 <style type="text/css">
   body.login div#login h1 a {
-  	background-image: url(<?php echo get_stylesheet_directory_uri(); ?>/images/logo.png);
+  	background-image: url(<?php the_field('company_name', 'option'); ?>);
   	background-size: 327px 67px;
   	width: 327px;
-  	height: 67px;
+  	height: 85px;
   }
 </style>
 <?php }
