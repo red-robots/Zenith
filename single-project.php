2bf6feff42d6c89d66eef675f99c3b46a384583b
reneeobrien <reneelobrien@gmail.com>
2017-08-24 14:34:56 -0400

    styles

--- single-project.php
+++ single-project.php
@@ -33,14 +33,14 @@ get_header(); ?>
 						<h1><?php the_title(); ?></h1>
 							<h2><?php the_field('project_location') ?></h2>
 								<div class="single-nav-right">
-									<?php next_post_link( '%link','Next' ); ?>
+									<?php next_post_link( '%link','Previous' ); ?>
 								</div>
 							<?php the_field('project_description'); ?>
 					</div>
 				
 					<div class="right-side-work">
 						<div class="single-nav-left">
-							<?php previous_post_link('%link','Previous'); ?>
+							<?php previous_post_link('%link','Next'); ?>
 						</div>
 						<?php 
 
