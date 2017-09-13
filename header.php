ffadc735b62a1542db507d18e047368c0ff6c54e
Fritz <fre.healy@gmail.com>
2017-09-13 09:18:59 -0400

    adding google code to header

--- header.php
+++ header.php
@@ -14,6 +14,7 @@
 <head>
 <meta charset="<?php bloginfo( 'charset' ); ?>">
 <meta name="viewport" content="width=device-width, initial-scale=1">
+<meta name="google-site-verification" content="iQmsq6o5HJ-JsS1dLd8nolctfVl76BokcAt4rOAQHTM" />
 <link rel="profile" href="http://gmpg.org/xfn/11">
 <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
 <link href="https://fonts.googleapis.com/css?family=Rubik" rel="stylesheet">
